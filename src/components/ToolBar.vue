a9c780d4c831d6ce704343f3a03dd4e18cfba441
HikariNoHana <ps1721998@gmail.com>
2020-05-19 15:47:38 +0530

    first commit

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-toolbar
+    color="white"
+  >
+ <v-toolbar-items class="head">
+    <v-app-bar-nav-icon @click="al"></v-app-bar-nav-icon>
+        <v-btn text>
+          DashBoard
+        </v-btn>
+
+        <v-btn text>
+            Users
+        </v-btn>
+
+        <v-btn text>
+          Settings
+        </v-btn>
+  </v-toolbar-items>
+     <v-spacer></v-spacer>
+   <v-toolbar-item>
+        <v-btn text>
+          Notification
+        </v-btn>
+  
+         <v-btn text @click="Invert">
+         Theme
+         </v-btn>
+  
+        <v-btn text>
+          Message
+        </v-btn>
+  
+        <v-btn text>
+          Account
+        </v-btn>
+
+      </v-toolbar-item>
+  </v-toolbar>
+</template>
+
+<script>
+  export default{
+    name :'ToolBar',
+    methods : {
+      al:function() {
+        alert('hii');
+      },
+      Invert:function(){
+        <v-toolbar>
+        color:black;</v-toolbar>
+      }
+    }
+  }
+</script>
+
